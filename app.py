0b19482ee8c69ee0f6835171aadfa4678cf7db44
Benoily <122854359+Benoily@users.noreply.github.com>
2023-03-06 13:23:48 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,40 +1,22 @@
-#+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-#|r|e|d|a|n|d|g|r|e|e|n|.|c|o|.|u|k|
-#+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-
-from flask import Flask, jsonify
-from flask_restful import Resource, Api, reqparse
-import nltk
-from nltk.sentiment.vader import SentimentIntensityAnalyzer
+from flask import Flask
+from flask_restful import Resource, Api
+import sys
+import os
 
 app = Flask(__name__)
 api = Api(app)
+port = 5000
 
-output = {}
+if sys.argv.__len__() > 1:
+    port = sys.argv[1]
+print("You said port is : {} ".format(port))
 
-# argument parsing
-parser = reqparse.RequestParser()
-parser.add_argument('q',help='Pass a sentence to analyse')
-
-class SentAnalysis(Resource):
-
+class HelloWorld(Resource):
     def get(self):
+        return {'hello': 'world Port : {} '.format(port)}
 
-        # use parser and find the user's query
-        args = parser.parse_args()
-        sentence = args['q']
+api.add_resource(HelloWorld, '/')
 
-        nltk.download("vader_lexicon")
-        sid = SentimentIntensityAnalyzer()
-        score = sid.polarity_scores(sentence)["compound"]
-        if score > 0:
-            output["sentiment"] = "Positive"    
-        else:
-            output["sentiment"] = "Negative"  
 
-        return jsonify(output)
-
-api.add_resource(SentAnalysis, '/')
-
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=port)
